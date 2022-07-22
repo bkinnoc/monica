1e5b4224575d421aa57d576aefa1f427a3bb8262
Malcolm Paul <malcolm@ninjasitm.com>
2022-07-22 10:52:36 -0400

    Adjusted the config for mailcow

--- config/mailcow.php
+++ config/mailcow.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'url' => env('MAILCOW_URL', 'localhost'),
+    'url' => env('MAILCOW_WEB', 'http://localhost:8480'),
     'domain' => env('MAILCOW_DOMAIN', 'localhost'),
-    'host' => env('MAILCOW_HOST', 'localhost'),
+    'host' => env('MAILCOW_HOST', 'http://localhost:8480'),
     'debug' => env('MAILCOW_DEBUG', false),
     'quota' => env('MAILCOW_QUOTA', 1024),
     'api_key' => env('MAILCOW_API_KEY'),
